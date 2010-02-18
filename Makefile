c563a457dffa9e42cc83cd4d7a1706e3c2ffd12f
dvd1985 <dvd1985@0fb79dcc-11e1-11df-9f1b-c989b4c7fc60>
2010-02-18 22:45:32 +0000

    Migrating de Flood Fill algorithm implementation to C.
    I've started recoding the Flood Fill algorithm to C, and creating a python wrapper so
    Painthon can use it.
    
    Right now, I have the wrapper configured properly, and a C skeletton which works. The
    algorithm itself, though, is not implemented. The skeletton performs a substitution
    of all the pixels in the image (fills the whole image with the given replacement color).

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-painthon: painthon.xml
+GCC=gcc
+LD=ld
+LIBS=
+
+painthon: painthon.xml floodfill.so
 
 painthon.xml: painthon.glade
 	@gtk-builder-convert painthon.glade painthon.xml
@@ -6,3 +10,12 @@ painthon.xml: painthon.glade
 	@mv /tmp/painthon.xml painthon.xml
 	@cat painthon.xml | sed -e "s/icon\">painthon.svg/icon\">pixmaps\/painthon.svg/" > /tmp/painthon.xml
 	@mv /tmp/painthon.xml painthon.xml
+
+floodfill.so: ./lib/c/floodfill.o
+	$(LD) -shared -soname $@ -o ./lib/c/$@ -lc ./lib/c/floodfill.o
+
+floodfill.o: ./lib/c/floodfill.c
+	$(GCC) -fPIC $(LIBS) -o ./lib/c/floodfill.o -c ./lib/c/floodfill.c
+
+clean:
+	@rm -f painthon.xml ./lib/c/floodfill.o ./lib/c/floodfill.so
---
+++ lib/c/algorithms.py
@@ -0,0 +1,35 @@
+from ctypes import cdll
+from ctypes import c_char_p
+from ctypes import c_int
+
+from lib.misc.utils import Callable
+
+class FloodFillAlgorithm:
+
+   def execute(x, y, image, width, height, bpc, replacement):
+      ireplacement = FloodFillAlgorithm.color_to_int(replacement)
+
+      lib="/home/dvd/painthon/lib/c/floodfill.so"
+      dll = cdll.LoadLibrary(lib)
+      function = (lambda x, y, image, width, height, bpc, ireplacement: \
+         dll.floodfill( \
+            c_int(x), \
+            c_int(y), \
+            c_char_p(image), \
+            c_int(width), \
+            c_int(height), \
+            c_int(bpc), \
+            c_int(ireplacement) \
+      )  )
+
+      function(x, y, image, width, height, bpc, ireplacement)
+   execute = Callable(execute)
+
+   def color_to_int(color):
+      res = 0
+      res |= (color[0] & 0xff) << 24
+      res |= (color[1] & 0xff) << 16
+      res |= (color[2] & 0xff) << 8
+      res |= (color[3] & 0xff)
+      return res
+   color_to_int = Callable(color_to_int)
