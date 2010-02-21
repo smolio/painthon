de1fc893a68f61210105f50ed2273edf9543b5cc
dvd1985 <dvd1985@0fb79dcc-11e1-11df-9f1b-c989b4c7fc60>
2010-02-21 21:53:48 +0000

    Floodfill algorithm in C completed.
    I've finally coded the floodfill algorithm in C. It now works pretty
    fast, and the code contains no bugs (appearently ;-)):
    
    I should now comment it a little bit, but it is not a priority.

--- lib/c/floodfill.c
+++ lib/c/floodfill.c
@@ -1,13 +1,114 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 typedef struct {
    int red;
    int green;
    int blue;
    int alpha;
 } color;
+
+// Definint a linked list structure
+struct node_st {
+   int x;
+   int y;
+   struct node_st *next;
+};
+typedef struct node_st node;
+
+
+// Functions
+int getIndex(int x, int y, int w, int bpc);
+color blend(color current, color replacement, int alpha);
+char compareUsingImage(int x, int y, char image[], int w, int h, int bpc, color target);
+char compareUsingColor(color current, color target);
+color getColor(int index, char image[], int bpc);
+color mkcolor(int red, char green, char blue, char alpha);
+color imkcolor(int thecolor);
+int floodfill(int x, int y, char image[], int w, int h, int bpc, int ireplacement);
+
+/*
+ * CODE STARTS HERE
+ */
+int floodfill(int x, int y, char image[], int w, int h, int bpc, int ireplacement) {
+   // Replacement color
+   color replacement = imkcolor(ireplacement);
+
+   // Target color
+   color target = getColor(getIndex(x, y, w, bpc), image, bpc);
+
+   if (compareUsingColor(target, replacement) == 0xff)
+      return 0;
+
+   // Creating the list/stack
+   node *list = (node*) malloc(sizeof(struct node_st));
+   (*list).x = x;
+   (*list).y = y;
+   (*list).next = NULL;
+
+   // The algorithm itself
+   int iterations=0;
+   while (list != NULL) {
+      node *pointer_to_free = list;
+      node current = *list;
+      list = current.next;
+
+      int index = getIndex(current.x, current.y, w, bpc);
+      color current_color = getColor(index, image, bpc);
+      int blending_alpha = compareUsingColor(current_color, target);
+      if (blending_alpha != 0) {
+         color result = blend(current_color, replacement, blending_alpha);
+         image[index] = result.red;
+         image[index + 1] = result.green;
+         image[index + 2] = result.blue;
+      }
+
+      // Query neighbours...
+      node *new;
+
+      // North
+      if (compareUsingImage(current.x, current.y-1, image, w, h, bpc, target)) {
+         new = (node*) malloc(sizeof(struct node_st));
+         (*new).x = current.x;
+         (*new).y = current.y-1;
+         (*new).next = list;
+         list = new;
+      }
+
+      // South
+      if (compareUsingImage(current.x, current.y+1, image, w, h, bpc, target)) {
+         new = (node*) malloc(sizeof(struct node_st));
+         (*new).x = current.x;
+         (*new).y = current.y+1;
+         (*new).next = list;
+         list = new;
+      }
+
+      // West
+      if (compareUsingImage(current.x-1, current.y, image, w, h, bpc, target)) {
+         new = (node*) malloc(sizeof(struct node_st));
+         (*new).x = current.x-1;
+         (*new).y = current.y;
+         (*new).next = list;
+         list = new;
+      }
+
+      // East
+      if (compareUsingImage(current.x+1, current.y, image, w, h, bpc, target)) {
+         new = (node*) malloc(sizeof(struct node_st));
+         (*new).x = current.x+1;
+         (*new).y = current.y;
+         (*new).next = list;
+         list = new;
+      }
+
+      free(pointer_to_free);
+      iterations ++; if (iterations == w*h*bpc*5) break;
+   }
+
+   return 0;
+}
+
 
 color imkcolor(int thecolor) {
    color result;
@@ -26,11 +127,36 @@ color mkcolor(int red, char green, char blue, char alpha) {
    x |= (green & 0xff) << 16;
    x |= (blue & 0xff) << 8;
    x |= (alpha & 0xff);
-   return imkcolor(0x00ff00ff);
+   return imkcolor(x);
+}
+
+int getIndex(int x, int y, int w, int bpc) {
+   return y*w*bpc + x*bpc;
 }
 
 
-int compare(color current, color target) {
+color getColor(int index, char image[], int bpc) {
+   int red, green, blue, alpha;
+   red = image[index];
+   green = image[index + 1];
+   blue = image[index + 2];
+   if (bpc == 4) alpha = image[index + 3];
+
+   return mkcolor(red, green, blue, alpha);
+}
+
+
+char compareUsingImage(int x, int y, char image[], int w, int h, int bpc, color target) {
+   if (x<0 || x>=w) return 0;
+   if (y<0 || y>=h) return 0;
+
+   color current = getColor(getIndex(x, y, w, bpc), image, bpc);
+
+   return compareUsingColor(current, target);
+}
+
+
+char compareUsingColor(color current, color target) {
    if (current.red == target.red &&
       current.green == target.green &&
       current.blue == target.blue)
@@ -55,42 +181,3 @@ color blend(color current, color replacement, int alpha) {
    return result;
 }
 
-
-int floodfill(int x, int y, char image[], int w, int h, int bpc, int ireplacement) {
-   int i, j;
-   int index, red, green, blue, alpha;
-
-   // Replacement color
-   color replacement = imkcolor(ireplacement);
-
-   // Target color
-   index = x*w*bpc + y*bpc;
-   red = image[index];
-   green = image[index + 1];
-   blue = image[index + 2];
-   if (bpc == 4) alpha = image[index + 3];
-   color target = mkcolor(red, green, blue, alpha);
-
-   // The algorithm itself
-   for (i=0; i<h; ++i) {
-      for (j=0; j<w; ++j) {
-         index = i*w*bpc + j*bpc;
-         red = image[index];
-         green = image[index + 1];
-         blue = image[index + 2];
-         if (bpc == 4) alpha = image[index + 3];
-
-         image[index] = 0x00;
-         color current = mkcolor(red, green, blue, alpha);
-
-         int blending_alpha = compare(current, target);
-         if (blending_alpha != 0) {
-            color result = blend(current, replacement, blending_alpha);
-            image[index] = result.red;
-            image[index + 1] = result.green;
-            image[index + 2] = result.blue;
-         }
-   }  }
-
-   return 0;
-}
